ffcca39d2ea9d8543ac4b5ce4f92cb6bbe617fac
gmachado <gmachado@student.42sp.org.br>
2023-11-12 21:14:50 -0300

    feat: Complete ex01.

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmachado <gmachado@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 04:44:48 by gmachado          #+#    #+#             */
+/*   Updated: 2023/11/12 21:14:06 by gmachado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) : value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed::Fixed(const int int_value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	value = int_value << fractional;
+}
+
+Fixed::Fixed(const float fp_value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	value = (fp_value * (1 << fractional) + (value >= 0 ? 0.5f : -0.5f));
+}
+
+Fixed &Fixed::operator=(const Fixed &src)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	value = src.getRawBits();
+	return *this;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	return value;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	value = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+	return ((float)value / (1 << fractional));
+}
+
+int Fixed::toInt(void) const
+{
+	return (value >> fractional);
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed)
+{
+	out << fixed.toFloat();
+	return out;
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmachado <gmachado@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 04:44:54 by gmachado          #+#    #+#             */
+/*   Updated: 2023/11/12 18:20:09 by gmachado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+	private:
+		int value;
+		static const int fractional = 8;
+	public:
+		Fixed(void);
+		Fixed(const Fixed &src);
+		Fixed(const int int_value);
+		Fixed(const float fp_value);
+
+		Fixed &operator=(const Fixed &src);
+
+		~Fixed(void);
+
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
+
+#endif
